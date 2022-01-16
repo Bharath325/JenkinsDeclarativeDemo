eafba629c474986aaa3c3616b8b1f12ed09382cb
002KBG744 <002KBG744@LAPTOP-I4NIJFCH>
2022-01-16 12:56:28 +0530

    First

---
+++ jenkinsDeclarativeDemo/script.groovy
@@ -0,0 +1,15 @@
+node{
+	for(int i=0; i<2; i++) {
+		stage 'Stage No.'+i
+		print 'Hello, world !'
+		if(i==0)
+		{
+			git "https://github.com/Bharath325/ForJeninks"
+			echo "Running on Stage No.0"
+		}
+		else {
+		build 'Declarative pipeline'
+		echo'Running on Stage No.1'
+		}
+	}
+}
